6e6f2251d39459c992549a79586ec59e285eb65b
BinBashBanana <51469593+BinBashBanana@users.noreply.github.com>
2020-03-02 23:31:39 -0800

    3-1-2020

--- gfiles/html5games/flappybird/__start__.js
+++ gfiles/html5games/flappybird/__start__.js
@@ -116,29 +116,43 @@
         return;
     }
 
-    app.configure(CONFIG_FILENAME, function (err) {
-        if (err) {
-            console.error(err);
-        }
-
-        configureCss(app._fillMode, app._width, app._height);
-        reflow();
-
-        window.addEventListener('resize', reflow, false);
-        window.addEventListener('orientationchange', reflow, false);
-
-        app.preload(function (err) {
+    var configure = function () {
+        app.configure(CONFIG_FILENAME, function (err) {
             if (err) {
                 console.error(err);
             }
 
-            app.loadScene(SCENE_PATH, function (err, scene) {
-                if (err) {
-                    console.error(err);
-                }
+            configureCss(app._fillMode, app._width, app._height);
 
-                app.start();
+            // do the first reflow after a timeout because of
+            // iOS showing a squished iframe sometimes
+            setTimeout(function () {
+                reflow();
+
+                window.addEventListener('resize', reflow, false);
+                window.addEventListener('orientationchange', reflow, false);
+
+                app.preload(function (err) {
+                    if (err) {
+                        console.error(err);
+                    }
+
+                    app.loadScene(SCENE_PATH, function (err, scene) {
+                        if (err) {
+                            console.error(err);
+                        }
+
+                        app.start();
+                    });
+                });
             });
         });
-    });
-}());
+    };
+
+    if (PRELOAD_MODULES.length > 0) {
+        loadModules(PRELOAD_MODULES, ASSET_PREFIX, configure);
+    } else {
+        configure();
+    }
+
+})();
